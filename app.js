16f7aec5ae2838e008620355e07d3b516412e6ba
Rúben ferreira <56252824+Diamanterub@users.noreply.github.com>
2022-03-01 22:15:00 +0000

    Structure Changes + Code improvements

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-require('dotenv').config({
-    debug: true
-})
-const express = require("express")
-const mongoose = require("mongoose")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-const port = process.env.Port || 3000;
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const port = process.env.Port || 3001;
+const users = require('./routes/route_users')
+const utilities = require('./utilities/utilities')
 
 const app = express()
 
@@ -17,6 +17,27 @@ app.use(express.json())
 
 const User = require("./models/User.js")
 
+const auth = function(req, res, next) {
+    let exceptions = ['/login', '/register']; 
+    if(exceptions.indexOf(req.url) >= 0) {
+        next(); 
+    } else {
+        utilities.validateToken(req.headers.authorization, (result) => {
+            if(result) {
+                next(); 
+            } else {
+                res.status(401).send("Invalid Token"); 
+            }
+        })
+    }
+}
+
+app.use(express.json());
+app.use(auth); 
+app.use('/', users)
+
+
+
 //Public Route
 app.get("/", (req, res) => {
     res.status(200).json({
@@ -24,133 +45,6 @@ app.get("/", (req, res) => {
     })
 })
 
-//Register User
-app.post("/auth/register", async (req, res) => {
-    const {
-        name,
-        email,
-        password,
-        confirmpassword
-    } = req.body
-
-    //Val
-    if (!name) {
-        return res.status(422).json({
-            msg: "Name is required"
-        })
-    }
-    if (!email) {
-        return res.status(422).json({
-            msg: "Email is required"
-        })
-    }
-    if (!password) {
-        return res.status(422).json({
-            msg: "Password is required"
-        })
-    }
-
-    if (password !== confirmpassword) {
-        return res.status(422).json({
-            msg: "Incorrect Password"
-        })
-    }
-
-    //Check if user exists on Database
-
-    const userExists = await User.findOne({
-        email: email
-    })
-    if (userExists) {
-        return res.status(422).json({
-            msg: "Email already taken"
-        })
-    }
-
-    //Create password
-    const salt = await bcrypt.genSalt(12)
-    const passwordHash = await bcrypt.hash(password, salt)
-
-    //Create User
-
-    const user = new User({
-        name,
-        email,
-        password: passwordHash,
-    })
-
-    try {
-        await user.save()
-        res.status(201).json({
-            msg: "User added to StockyDB"
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({
-            msg: "Error detected"
-        })
-
-    }
-})
-
-app.post("/auth/login", async (req, res) => {
-
-    const {
-        email,
-        password
-    } = req.body
-
-    //Val
-    if (!email) {
-        return res.status(422).json({
-            msg: "Email is required"
-        })
-    }
-    if (!password) {
-        return res.status(422).json({
-            msg: "Password is required"
-        })
-    }
-
-    //Check if user exists on Database
-
-    const user = await User.findOne({
-        email: email
-    })
-    if (!user) {
-        return res.status(404).json({
-            msg: "User doesn't exists"
-        })
-    }
-
-    //Check if password matches
-
-    const checkPassword = await bcrypt.compare(password, user.password)
-
-    if (!checkPassword) {
-        return res.status(422).json({
-            msg: "Invalid Password"
-        })
-    }
-
-    try {
-        const secret = process.env.SECRET
-
-        const token = jwt.sign({
-            id: user._id
-        }, secret)
-        
-        res.status(200).json({msg : `Login Successful`,token})
-
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({
-            msg: "Error detected"
-        })
-
-    }
-
-})
 
 //Credentials
 const dbUser = process.env.DB_USER
@@ -161,3 +55,7 @@ mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.xoha1.mongodb.net/m
     app.listen(3000)
     console.log("Connected to the StockyDB")
 }).catch((err) => console.log(err))
+
+app.listen(port, function() {
+    console.log("App is running on port " + port)
+})  
